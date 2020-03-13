d37183af43aeca23b5b241025727147c15cdf7be
Casper Welzel Andersen <casper.andersen@epfl.ch>
2020-03-13 12:28:06 +0100

    Pass path/file to run_tests.sh
    To determine whether the input is a docker image tag name or a
    path/file, it does a simple check for the presence of '.' or '/' in the
    input parameter

--- run_tests.sh
+++ run_tests.sh
@@ -1,9 +1,24 @@
 #!/bin/sh
 
 DOCKER_BATS_IMAGE_NAME=optimade_bats
+DOCKER_BATS_TEST_PATH=.
 
 if [ -n "$1" ]; then
-    DOCKER_BATS_IMAGE_NAME=$1
+    if echo "$1" | grep -Eq '*[/\.]+.*'; then
+        # TEST PATH
+        DOCKER_BATS_TEST_PATH=$1
+        if [ -n "$2" ]; then
+            # Assume IMAGE NAME
+            DOCKER_BATS_IMAGE_NAME=$2
+        fi
+    else
+        # IMAGE NAME
+        DOCKER_BATS_IMAGE_NAME=$1
+        if [ -n "$2" ]; then
+            # Assume TEST PATH
+            DOCKER_BATS_TEST_PATH=$2
+        fi
+    fi
 fi
 
-docker run -i -v "$(pwd):/code" --workdir /code/tests $DOCKER_BATS_IMAGE_NAME .
+docker run -i -v "$(pwd):/code" --workdir /code/tests $DOCKER_BATS_IMAGE_NAME $DOCKER_BATS_TEST_PATH
