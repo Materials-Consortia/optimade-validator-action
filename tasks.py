6c6d5d2bb2b2a712d390536d8d14f148fd6d22d6
Casper Welzel Andersen <43357585+CasperWA@users.noreply.github.com>
2021-11-03 11:53:28 +0100

    Add pre-commit (#77)
    Add pre-commit hooks and Python deps file.
    
    Update workflows with new jobs and deps source.
    
    New invoke task to ensure the correct Python version throughout.
    Implement usage of the new invoke task when activating auto-merge for
    Dependabot PRs.
    
    Simplify list of version creation.
    
    Co-authored-by: Johan Bergsma <JPBergsma@users.noreply.github.com>

--- tasks.py
+++ tasks.py
@@ -1,35 +1,48 @@
+"""Invoke tasks for managing the repository."""
 from pathlib import Path
 import re
 import sys
-from typing import Tuple
+from typing import TYPE_CHECKING
 
 try:
     from invoke import task
 except ImportError:
     sys.exit("'invoke' MUST be installed to run these tasks.")
 
+if TYPE_CHECKING:
+    from typing import Tuple, Union
+
 
 TOP_DIR = Path(__file__).parent.resolve()
 
 
-def update_file(filename: str, sub_line: Tuple[str, str], strip: str = None):
-    """Utility function for tasks to read, update, and write files"""
-    with open(filename, "r") as handle:
-        lines = [
-            re.sub(sub_line[0], sub_line[1], line.rstrip(strip)) for line in handle
-        ]
+def update_file(
+    filename: "Union[Path, str]", sub_line: "Tuple[str, str]", strip: str = None
+) -> None:
+    """Utility function for tasks to read, update, and write files."""
+    if isinstance(filename, str):
+        filename = Path(filename)
 
-    with open(filename, "w") as handle:
-        handle.write("\n".join(lines))
-        handle.write("\n")
+    if not filename.exists():
+        raise FileNotFoundError(f"Could not find file to be updated: {filename}")
+
+    lines = [
+        re.sub(sub_line[0], sub_line[1], line.rstrip(strip))
+        for line in filename.read_text(encoding="utf8").splitlines()
+    ]
+
+    filename.write_text("\n".join(lines) + "\n", encoding="utf8")
 
 
 @task(help={"ver": "OPTIMADE Validator Action version to set"})
 def setver(_, ver=""):
-    """Sets the OPTIMADE Validator Action version"""
+    """Sets the OPTIMADE Validator Action version."""
     match = re.fullmatch(r"v?([0-9]+\.[0-9]+\.[0-9]+)", ver)
     if not match or (match and len(match.groups()) != 1):
-        sys.exit("Error: Please specify version as 'Major.Minor.Patch' or 'vMajor.Minor.Patch'")
+        sys.exit(
+            "Error: Please specify version as 'Major.Minor.Patch' or "
+            "'vMajor.Minor.Patch'"
+        )
     ver = match.group(1)
 
     major_version = ver.split(".")[0]
@@ -37,12 +50,46 @@ def setver(_, ver=""):
         "https://github.com/Materials-Consortia/optimade-validator-action/releases/tag/"
     )
     update_file(
-        TOP_DIR.joinpath("README.md"),
+        TOP_DIR / "README.md",
         (
-            fr"`v{major_version}` \| \[`v{major_version}(\.[0-9]+){{2}}.*v{major_version}(\.[0-9]+){{2}}",
+            (
+                fr"`v{major_version}` \| \[`v{major_version}(\.[0-9]+){{2}}.*"
+                fr"v{major_version}(\.[0-9]+){{2}}"
+            ),
             f"`v{major_version}` | [`v{ver}`]({tag_url}v{ver}",
         ),
         strip="\n",
     )
 
     print(f"Bumped version to {ver}")
+
+
+@task
+def check_dockerfile_python_version(_):
+    """Check CI and config files are using the same Python version as in the
+    Dockerfile."""
+    with (TOP_DIR / "Dockerfile").open() as handle:
+        for line in handle:
+            match = re.match(
+                r".*python:(?P<version>.*)-slim-buster.*",
+                line,
+            )
+            if match:
+                dockerfile_python_version: str = match.group("version")
+                break
+        else:
+            sys.exit("Couldn't determine the Python version used in Dockerfile.")
+
+    update_file(
+        TOP_DIR / ".github" / "workflows" / "ci_tests.yml",
+        (r"PYTHON_VERSION: 3\.[0-9]+", f"PYTHON_VERSION: {dockerfile_python_version}"),
+    )
+    update_file(
+        TOP_DIR / "pyproject.toml",
+        (r"python_version = \".*\"", f'python_version = "{dockerfile_python_version}"'),
+    )
+
+    print(
+        "Successfully updated CI and config files to use Python "
+        f"{dockerfile_python_version}"
+    )
--- tests/test_validator_version.bats
+++ tests/test_validator_version.bats
@@ -1,5 +1,3 @@
-#!/usr/bin/env bats
-
 load 'test_fixtures'
 
 @test "validator_version='latest'" {
