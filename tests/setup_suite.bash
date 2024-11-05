dfea968af0f03a2495f0d0048ac37c0ad9928b8d
Casper Welzel Andersen <casper.w.andersen@sintef.no>
2024-11-05 09:38:32 +0100

    Also fix that env in the setup_suite.bash file

--- tests/setup_suite.bash
+++ tests/setup_suite.bash
@@ -2,7 +2,11 @@
 
 # Set workdir to Docker default if not set already
 if [ -z "${DOCKER_BATS_WORKDIR}" ]; then
-    export DOCKER_BATS_WORKDIR=/code
+    if [ -n "${CI}" ]; then
+        export DOCKER_BATS_WORKDIR=${GITHUB_WORKSPACE}
+    else
+        export DOCKER_BATS_WORKDIR=/code
+    fi
 fi
 
 # Absolute path to entrypoint.sh
